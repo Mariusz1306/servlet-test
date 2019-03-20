5b8ef35d6a5d84e68970b348829acb20162afb9a
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2019-03-20 17:44:21 +0100

    Add files via upload

--- src/MyServlet.java
+++ src/MyServlet.java
@@ -1,11 +1,13 @@
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MyServlet extends HttpServlet {
+
+    String msg;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -16,6 +18,84 @@ public class MyServlet extends HttpServlet {
 
     void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        Cookie[] cookies = request.getCookies();
+        Cookie licznik = null;
+
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++){
+                if (cookies[i].getName().equals("licznik")) {
+                    licznik = cookies[i];
+                    break;
+                }
+            }
+        }
+
+        if (licznik == null) {
+            licznik = new Cookie("licznik", "0");
+        } else {
+            Integer v = Integer.parseInt(licznik.getValue());
+            v++;
+            licznik.setValue(v+"");
+        }
+
+        licznik.setMaxAge(-1);
+        response.addCookie(licznik);
+
+        out.println(licznik.getValue());
+
+        try {
+            HttpSession session = request.getSession(true);
+            String action = request.getParameter("akcja");
+
+            if (action != null) {
+                if (action.equals("wyloguj")) {
+                    Boolean wartosc = false;
+                    session.setAttribute("zalogowany", wartosc);
+                }
+            }
+            Boolean loggedIn = (Boolean)session.getAttribute("zalogowany");
+
+            if (loggedIn == null)
+                loggedIn = false;
+            if (loggedIn == true) {
+                out.println("<h1>ZALOGOWANY</h1>");
+                out.println("<form method = \"get\">");
+                out.println("<input type = \"hidden\" name = \"akcja\" value = \"wyloguj\" \\>");
+                out.println("<input type = \"submit\" value = \"Wyloguj\" \\>");
+                out.println("</form>");
+            } else {
+                out.println("<form method=\"get\">");
+                out.println("<input type=\"text\" name=\"user\"/>");
+                out.println("<input type=\"password\" name=\"pass\"/>");
+                out.println("<input type=\"submit\" value=\"zaloguj\"/>");
+                out.println("</form>");
+
+                String user;
+                String pass;
+                user = request.getParameter("user");
+                pass = request.getParameter("pass");
+                if (user != null && pass != null) {
+                    if (user.equals("radek") && pass.equals("haslo")) {
+                        loggedIn = true;
+                        session.setAttribute("zalogowany", loggedIn);
+                        response.setIntHeader("Refresh", 0);
+                    }
+                }
+
+            }
+
+
+        } finally {
+            out.close();
+        }
+
+
+
+
+        /*response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
             out.println("<h2>Welcome to my first servlet application in IntelliJ</h2>");
@@ -27,12 +107,25 @@ public class MyServlet extends HttpServlet {
             out.println("Wynik getHeader: " + request.getHeader("Accept-Encoding") + "<BR>");
             out.println("Wynik getHeader: " + request.getHeader("User-Agent") + "<BR>");
             out.println("<BR>");
-            if (request.getParameter("imie") != null && request.getParameter("wiek") !=null) {
-                out.println("Wynik getParameter(\"imie\"): : " + request.getParameter("imie") + "<BR>");
-                out.println("Wynik getParameter(\"wiek\"): : " + request.getParameter("wiek") + "<BR>");
+            try {
+                if (request.getParameter("x") != null && request.getParameter("y") != null) {
+                    out.println("Wynik getParameter(\"x\"): : " + request.getParameter("x") + "<BR>");
+                    out.println("Wynik getParameter(\"y\"): : " + request.getParameter("y") + "<BR>");
+                    int x, y;
+                    x = Integer.parseInt(request.getParameter("x"));
+                    y = Integer.parseInt(request.getParameter("y"));
+                    out.println("Wynik dodawania: " + (x + y) + "<BR>");
+                }
+            } catch (NumberFormatException e){
+                out.println("Incorrect input. " + e.getMessage() + "<BR>");
             }
         } finally {
             out.close();
-        }
+        }*/
     }
+
+    public void init(ServletConfig config) {
+        msg = config.getInitParameter("name");
+    }
+
 }
